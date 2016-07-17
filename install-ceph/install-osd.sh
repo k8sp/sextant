93c9372880130fd3b6cee5b91e2ce4513df7eb03
Qingsong Liu <liuqs.ustc@gmail.com>
2016-07-17 10:49:38 +0800

    Run OSD daemon for non-boot disk automatically

--- install-ceph/install-osd.sh
+++ install-ceph/install-osd.sh
@@ -14,7 +14,14 @@ do
   etcdctl get /unisound/ceph-dist$f | base64 --decode | sudo tee $f >/dev/null
 done
 
-# Run an OSD daemon, use /dev/sdb for now
-docker run -d --net=host -v /etc/ceph:/etc/ceph -v /var/lib/ceph:/var/lib/ceph \
-  -v /dev:/dev --privileged=true -e OSD_FORCE_ZAP=1 \
-  -e OSD_DEVICE=/dev/sdb ceph/daemon osd_ceph_disk
+
+devices=$(sudo parted -l |grep 'Disk' |awk '!/boot/{if(NR%2 == 0) print x};{x=$2}'|sed 's/:$//')
+
+# Run OSD daemon for each device
+for device in devices
+do
+  echo "Run OSD daemon on: $device"
+  docker run -d --net=host -v /etc/ceph:/etc/ceph -v /var/lib/ceph:/var/lib/ceph \
+    -v /dev:/dev --privileged=true -e OSD_FORCE_ZAP=1 \
+    -e OSD_DEVICE=$device ceph/daemon osd_ceph_disk
+done
