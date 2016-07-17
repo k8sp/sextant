adbd24a3a0794437306b37b8edd720ba2eca4654
Li Peng <lipeng@unisound.com>
2016-07-17 22:06:06 +0800

    Add --pid=host to run ceph osd
    See https://goo.gl/cFilnr and
    https://github.com/ceph/ceph-docker/issues/19 for the explanation.

--- install-ceph/install-osd.sh
+++ install-ceph/install-osd.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 # Run an OSD daemon, use /dev/sdb for now
-docker run -d --net=host -v /etc/ceph:/etc/ceph -v /var/lib/ceph:/var/lib/ceph \
-  -v /dev:/dev --privileged=true -e KV_TYPE=etcd -e OSD_FORCE_ZAP=1 \
+docker run -d --pid=host --net=host \
+  -v /etc/ceph:/etc/ceph-v /var/lib/ceph:/var/lib/ceph \
+  -v /dev:/dev --privileged=true \
+  -e KV_TYPE=etcd \
+  -e OSD_FORCE_ZAP=1 \
   -e OSD_DEVICE=/dev/sdb ceph/daemon osd_ceph_disk
