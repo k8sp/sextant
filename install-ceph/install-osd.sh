40ecd4ed9cd49689ab4ee0544005ad9c976209f0
Li Peng <lipeng@unisound.com>
2016-07-17 22:32:10 +0800

    Fix a bug

--- install-ceph/install-osd.sh
+++ install-ceph/install-osd.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 # Run an OSD daemon, use /dev/sdb for now
-docker run -d --pid=host --net=host \
-  -v /etc/ceph:/etc/ceph-v /var/lib/ceph:/var/lib/ceph \
-  -v /dev:/dev --privileged=true \
+docker run -d --pid=host --net=host --privileged=true \
+  -v /etc/ceph:/etc/ceph \
+  -v /var/lib/ceph:/var/lib/ceph \
+  -v /dev:/dev \
   -e KV_TYPE=etcd \
   -e OSD_FORCE_ZAP=1 \
   -e OSD_DEVICE=/dev/sdb ceph/daemon osd_ceph_disk
