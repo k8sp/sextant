d93323fdaac0ad9a6c209ebdac372b1b14756cf2
Qingsong Liu <liuqs.ustc@gmail.com>
2016-07-21 17:35:07 +0800

    Use lsblk cmd to find non-system disks

--- install-ceph/install-osd.sh
+++ install-ceph/install-osd.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
-devices=$(sudo parted -l |grep 'Disk' |awk '!/boot/{if(NR%2 == 0) print x};{x=$2}'|sed 's/:$//')
+#Obtain devices
+devices=$(lsblk -l |awk '$6=="disk"{print $1}')
+systemdevice=$(lsblk -l |awk '$7=="/usr"{print $1}' |sed 's/[0-9]\+$//')
 
 # Run OSD daemon for each device
-for device in devices
+for a in $devices
 do
-  echo "Run OSD daemon on: $device"
-  docker run -d --pid=host --net=host --privileged=true \
-    -v /etc/ceph:/etc/ceph \
-    -v /var/lib/ceph:/var/lib/ceph \
-    -v /dev:/dev \
-    -e KV_TYPE=etcd \
-    -e OSD_FORCE_ZAP=1 \
-    -e OSD_DEVICE=$device \
-    ceph/daemon osd_ceph_disk
+  if [[ $a != $systemdevice ]]; then
+    device="/dev/$a"
+    echo "Run OSD daemon on: $device"
+    docker run -d --pid=host --net=host --privileged=true \
+      -v /etc/ceph:/etc/ceph \
+      -v /var/lib/ceph:/var/lib/ceph \
+      -v /dev:/dev \
+      -e KV_TYPE=etcd \
+      -e OSD_FORCE_ZAP=1 \
+      -e OSD_DEVICE=$device \
+      ceph/daemon osd_ceph_disk
+  fi
 done
