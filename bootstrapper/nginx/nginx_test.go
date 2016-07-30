d44b8fb5b7da634c3a8a459741fd8a158ba48448
Qingsong Liu <liuqingsong@unisound.com>
2016-07-30 22:57:38 +0800

    Fix "not running" status on Ubuntu

--- bootstrapper/nginx/nginx_test.go
+++ bootstrapper/nginx/nginx_test.go
@@ -31,7 +31,7 @@ func TestInstall(t *testing.T) {
 			// prevents us from starting nginx service in a CentOS docker container.
 		case "ubuntu":
 			l := <-sh.Head(sh.Run("service", "nginx", "status"), 1)
-			if !strings.Contains(l, "running") {
+			if strings.Contains(l, "not running") || !strings.Contains(l, "running") {
 				t.Errorf("nginx service is not running: %s", l)
 			}
 		}
