8394ea871a041a6ad76b1540b554286f2087723c
Yi Wang <yi.wang.2005@gmail.com>
2016-07-31 00:39:19 -0700

    Add coreos package

--- bootstrapper/coreos/coreos.go
+++ bootstrapper/coreos/coreos.go
@@ -2,20 +2,54 @@ package coreos
 
 import (
 	"fmt"
-	"log"
-	"strings"
+	"path"
 
+	"github.com/k8sp/auto-install/bootstrapper/cmd"
 	"github.com/wangkuiyi/sh"
 )
 
 // GetVersion returns the most recent version of the specified CoreOS
-// channel. channel could be "stable", "beta", or "alpha".
+// channel. channel could be "stable", "beta", or "alpha".  GetVersion
+// requires that curl has been installed.
 func GetVersion(channel string) string {
 	url := fmt.Sprintf("https://%s.release.core-os.net/amd64-usr/current/version.txt", channel)
-	ver := <-sh.Grep(sh.Run("curl", url), "COREOS_VERSION=")
-	fs := strings.Split(ver, "=")
-	if len(fs) != 2 {
-		log.Panicf("Unknown version line from %s: %s", url, ver)
-	}
-	return fs[1]
+	return <-sh.Cut(sh.Grep(sh.Run("curl", "-s", url), "COREOS_VERSION="), 2, "=")
+}
+
+// DownloadBootImage requires that curl and gnupg have been installed.
+func DownloadBootImage(channel, outDir string) {
+	ver := GetVersion(channel)
+
+	img := "coreos_production_pxe.vmlinuz"
+	cpio := "coreos_production_pxe_image.cpio.gz"
+	pkey := "CoreOS_Image_Signing_Key.asc"
+
+	// Download image files.
+	cmd.Run("curl", "-o",
+		path.Join(outDir, img),
+		fmt.Sprintf("https://%s.release.core-os.net/amd64-usr/%s/%s", channel, ver, img))
+	cmd.Run("curl", "-o",
+		path.Join(outDir, cpio),
+		fmt.Sprintf("https://%s.release.core-os.net/amd64-usr/%s/%s", channel, ver, cpio))
+
+	// Download signatures.
+	cmd.Run("curl", "-o",
+		path.Join(outDir, img+".sig"),
+		fmt.Sprintf("https://%s.release.core-os.net/amd64-usr/%s/%s.sig", channel, ver, img))
+	cmd.Run("curl", "-o",
+		path.Join(outDir, cpio+".sig"),
+		fmt.Sprintf("https://%s.release.core-os.net/amd64-usr/%s/%s.sig", channel, ver, cpio))
+
+	// Download the public key.
+	cmd.Run("curl", "-o",
+		path.Join(outDir, pkey),
+		fmt.Sprintf("https://coreos.com/security/image-signing-key/%s", pkey))
+
+	// Import the public key.
+	cmd.Run("gpg", "--import", "--keyid-format", "LONG", path.Join(outDir, pkey))
+
+	// Verify downloaded images.
+	cmd.Run("gpg", "--verify", path.Join(outDir, img+".sig"), path.Join(outDir, img))
+	cmd.Run("gpg", "--verify", path.Join(outDir, cpio+".sig"), path.Join(outDir, cpio))
+
 }
--- bootstrapper/coreos/coreos_test.go
+++ bootstrapper/coreos/coreos_test.go
@@ -1,10 +1,19 @@
 package coreos
 
 import (
+	"flag"
+	"log"
 	"strings"
 	"testing"
 
+	"github.com/k8sp/auto-install/bootstrapper/cmd"
+	"github.com/k8sp/auto-install/config"
 	"github.com/stretchr/testify/assert"
+)
+
+var (
+	indocker = flag.Bool("indocker", false,
+		"Tells that the test is running in a Docker container by dhcp_test.bash.")
 )
 
 func TestGetVersion(t *testing.T) {
@@ -13,4 +22,25 @@ func TestGetVersion(t *testing.T) {
 	stable := GetVersion("stable")
 	assert.True(t, strings.Compare(stable, beta) <= 0)
 	assert.True(t, strings.Compare(beta, alpha) <= 0)
+}
+
+func TestDownloadBootImage(t *testing.T) {
+	if *indocker {
+		const (
+			centos = "centos"
+			ubuntu = "ubuntu"
+		)
+
+		switch dist := config.LinuxDistro(); dist {
+		case centos:
+			cmd.Run("yum", "-y", "install", "curl")
+		case ubuntu:
+			cmd.Run("apt-get", "update")
+			cmd.Run("apt-get", "-y", "install", "curl")
+		default:
+			log.Panicf("Unsupported OS: %s", dist)
+		}
+
+		DownloadBootImage("stable", "/tmp")
+	}
 }
