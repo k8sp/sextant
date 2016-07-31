af69fbd51e0251e1d83cc49e1d758c6cec5de289
Yi Wang <yi.wang.2005@gmail.com>
2016-07-31 15:19:07 -0700

    Add RunWithEnv and TryWithEnv

--- bootstrapper/cmd/cmd.go
+++ bootstrapper/cmd/cmd.go
@@ -16,16 +16,37 @@ var (
 // log.Panic the stdout and stderr of the command, only if the
 // execution goes wrong.
 func Run(name string, arg ...string) {
-	run(true, name, arg)
+	run(true, nil, name, arg)
 }
 
+// Try works like Run, but doesn't panic if the commands returns a
+// non-zero value.
 func Try(name string, arg ...string) {
-	run(false, name, arg)
+	run(false, nil, name, arg)
 }
 
-func run(panic bool, name string, arg []string) {
+// RunWithEnv works like Run, but allows the user to set the environment
+// variables, in addition to those inherited from the parent process.
+func RunWithEnv(env []string, name string, arg ...string) {
+	run(true, env, name, arg)
+}
+
+// TryWithEnv works like Try, but allows the user to set the environment
+// variables, in addition to those inherited from the parent process.
+func TryWithEnv(env []string, name string, arg ...string) {
+	run(false, env, name, arg)
+}
+
+func run(panic bool, env []string, name string, arg []string) {
 	log.Printf("Running %s %s ...", name, strings.Join(arg, " "))
 	cmd := exec.Command(name, arg...)
+
+	for _, en := range os.Environ() {
+		cmd.Env = append(cmd.Env, en)
+	}
+	for _, en := range env {
+		cmd.Env = append(cmd.Env, en)
+	}
 
 	p := log.Printf
 	if panic {
--- bootstrapper/cmd/cmd_test.go
+++ bootstrapper/cmd/cmd_test.go
@@ -1,14 +1,29 @@
 package cmd
 
 import (
+	"fmt"
+	"io/ioutil"
+	"path"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestRun(t *testing.T) {
+func TestRunAndTry(t *testing.T) {
 	*Silent = true
 	assert.NotPanics(t, func() { Run("ls", "/") })
 	assert.Panics(t, func() { Run("something-not-exists") })
 	assert.NotPanics(t, func() { Try("something-not-exists") })
+}
+
+func TestRunWithEnv(t *testing.T) {
+	tmpdir, _ := ioutil.TempDir("", "")
+	tmpfile := path.Join(tmpdir, "TestRunWithEnv")
+
+	RunWithEnv([]string{"GOPATH=/tmp"},
+		"awk",
+		fmt.Sprintf("BEGIN{print ENVIRON[\"GOPATH\"] > \"%s\";}", tmpfile))
+
+	b, _ := ioutil.ReadFile(tmpfile)
+	assert.Equal(t, "/tmp\n", string(b))
 }
