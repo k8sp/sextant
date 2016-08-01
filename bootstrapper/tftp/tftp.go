a7f72762d14aa036a99ac90199e079f0257406cf
sunxingshuai <sunxingshuai@unisound.com>
2016-08-01 16:38:50 +0800

    mv to  bootstrapper/tftp/

--- bootstrapper/tftp/tftp.go
+++ bootstrapper/tftp/tftp.go
@@ -1,15 +1,14 @@
 package tftp
 
 import (
-	//"os/exec"
 	"log"
 
 	"github.com/k8sp/auto-install/bootstrapper/cmd"
 	"github.com/k8sp/auto-install/config"
-	//"github.com/topicai/candy"
 )
 
-func Tftp_install() {
+// Install TFTP service on the bootstrapper server
+func Install() {
 	const (
 		centos = "centos"
 		ubuntu = "ubuntu"
@@ -22,24 +21,19 @@ func Tftp_install() {
 
 	switch dist {
 	case centos:
+                cmd.Run("yum", "-y", "install", "xinetd")
 		cmd.Run("yum", "-y", "install", "tftp-server")
 	case ubuntu:
 		cmd.Run("apt-get", "update")
 		cmd.Run("apt-get", "-y", "install", "tftp-hpa")
 	}
 
-	// Note that the installation of nginx packages should have
-	// created directory /etc/tftp.
-	/*candy.WithCreated("/etc/tftp/tftp.conf", func(w io.Writer) {
-		_, e := fmt.Fprint(w, Conf(tmpl, c))
-		candy.Must(e)
-	})*/
-
 	switch dist {
 	case ubuntu:
 		cmd.Run("service", "tftpd-hpa", "restart")
 	case centos:
-		cmd.Run("chkconfig", "tftp", "xinetd", "on")
+                cmd.Run("chkconfig", "tftp", "on")
+		cmd.Run("chkconfig", "xinetd", "on")
 		cmd.Run("service", "xinetd", "restart")
 	}
 
