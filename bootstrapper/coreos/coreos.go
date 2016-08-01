c6b044bcaf36afcaf7a1f5160ee3448a963b8c99
Yi Wang <yi.wang.2005@gmail.com>
2016-07-31 21:42:58 -0700

    In resposne to comments from QingSong

--- bootstrapper/coreos/coreos.go
+++ bootstrapper/coreos/coreos.go
@@ -2,49 +2,57 @@ package coreos
 
 import (
 	"fmt"
+	"os"
 	"path"
 
 	"github.com/k8sp/auto-install/bootstrapper/cmd"
+	"github.com/k8sp/auto-install/config"
+	"github.com/topicai/candy"
 	"github.com/wangkuiyi/sh"
 )
 
-// GetVersion returns the most recent version of the specified CoreOS
-// channel. channel could be "stable", "beta", or "alpha".  GetVersion
-// requires that curl has been installed.
-func GetVersion(channel string) string {
+func version(channel string) (string, string) {
+	if channel == "" {
+		channel = "stable"
+	}
 	url := fmt.Sprintf("https://%s.release.core-os.net/amd64-usr/current/version.txt", channel)
-	return <-sh.Cut(sh.Grep(sh.Run("curl", "-s", url), "COREOS_VERSION="), 2, "=")
+	return channel, <-sh.Cut(sh.Grep(sh.Run("curl", "-s", url), "COREOS_VERSION="), 2, "=")
 }
 
 // DownloadBootImage requires that curl and gnupg have been installed.
-func DownloadBootImage(channel, outDir string) {
-	ver := GetVersion(channel)
+// Parameter channel could be "stable", "beta", or "alpha".  version
+// requires that curl has been installed.
+func DownloadBootImage(c *config.Cluster) {
+	channel, ver := version(c.CoreOSChannel)
+
+	dir := path.Join(c.NginxRootDir, ver)
+	candy.Must(os.MkdirAll(dir, 0644))
 
 	img := "coreos_production_pxe.vmlinuz"
 	cpio := "coreos_production_pxe_image.cpio.gz"
 	pkey := "CoreOS_Image_Signing_Key.asc"
 
 	// Download image files.
-	cmd.Run("curl", "-s", "-o", path.Join(outDir, img),
+	cmd.Run("curl", "-s", "-o", path.Join(dir, img),
 		fmt.Sprintf("https://%s.release.core-os.net/amd64-usr/%s/%s", channel, ver, img))
-	cmd.Run("curl", "-s", "-o", path.Join(outDir, cpio),
+	cmd.Run("curl", "-s", "-o", path.Join(dir, cpio),
 		fmt.Sprintf("https://%s.release.core-os.net/amd64-usr/%s/%s", channel, ver, cpio))
 
 	// Download signatures.
-	cmd.Run("curl", "-s", "-o", path.Join(outDir, img+".sig"),
+	cmd.Run("curl", "-s", "-o", path.Join(dir, img+".sig"),
 		fmt.Sprintf("https://%s.release.core-os.net/amd64-usr/%s/%s.sig", channel, ver, img))
-	cmd.Run("curl", "-s", "-o", path.Join(outDir, cpio+".sig"),
+	cmd.Run("curl", "-s", "-o", path.Join(dir, cpio+".sig"),
 		fmt.Sprintf("https://%s.release.core-os.net/amd64-usr/%s/%s.sig", channel, ver, cpio))
 
 	// Download the public key.
-	cmd.Run("curl", "-s", "-o", path.Join(outDir, pkey),
+	cmd.Run("curl", "-s", "-o", path.Join(dir, pkey),
 		fmt.Sprintf("https://coreos.com/security/image-signing-key/%s", pkey))
 
 	// Import the public key.
-	cmd.Run("gpg", "--import", "--keyid-format", "LONG", path.Join(outDir, pkey))
+	cmd.Run("gpg", "--import", "--keyid-format", "LONG", path.Join(dir, pkey))
 
 	// Verify downloaded images.
-	cmd.Run("gpg", "--verify", path.Join(outDir, img+".sig"), path.Join(outDir, img))
-	cmd.Run("gpg", "--verify", path.Join(outDir, cpio+".sig"), path.Join(outDir, cpio))
+	cmd.Run("gpg", "--verify", path.Join(dir, img+".sig"), path.Join(dir, img))
+	cmd.Run("gpg", "--verify", path.Join(dir, cpio+".sig"), path.Join(dir, cpio))
 
 }
