bc8dc206e2815ff7d15d55ee12eef27ae4a086ac
Yi Wang <yiwang01@baidu.com>
2016-08-19 14:15:04 -0700

    Make Dockerfile entrypoint start.sh, that runs all services

---
+++ bsroot/bsroot/start.sh
@@ -0,0 +1,17 @@
+#!/bin/ash
+
+# The trailing & runs cloud-config-server in background.
+/bsroot/cloud-config-server \
+    --cluster-desc-url="" \
+    --cluster-desc-file=/bsroot/cluster-desc.yml \
+    --cc-template-url="" \
+    --cc-template-file=/bsroot/cloud-config.template \
+    --ca-crt=/bsroot/ca.crt \
+    --ca-key=/bsroot/ca.key \
+    --dir=/bsroot/www & 
+
+# Without an explicity -k flag, dnsmasq runs in background by default.
+/usr/sbin/dnsmasq -C /bsroot/dnsmasq.conf 
+
+# TODO: to make registry run in background, need a trailing #.
+/go/bin/registry serve /bsroot/registry.yml
