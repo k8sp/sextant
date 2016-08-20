da874b9c97ee6ea10f6679faa1218f5329356368
Yi Wang <yiwang01@baidu.com>
2016-08-20 04:18:48 -0700

    Fix no ifconfig problem

--- bsroot/www/install-coreos.sh
+++ bsroot/www/install-coreos.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-mac_addr=`ifconfig | grep -A2 'broadcast' | grep -o '..:..:..:..:..:..'`
+mac_addr=`ip a | grep -A2 BROADCAST | grep -o 'link/ether ..:..:..:..:..:.. ' | grep -o '..:..:..:..:..:..'`
 wget http://192.168.50.4:8080/cloud-config/${mac_addr}.yml
 sudo coreos-install -d /dev/sda -c ${mac_addr}.yml -b http://192.168.50.4:8080/static
 sudo reboot
