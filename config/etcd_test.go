7ab8169bbff27aee95610e72751c170ce1b6c951
Yancey1989 <yancey1989@gmail.com>
2016-08-21 11:14:50 +0800

    etcd test fix hostname

--- config/etcd_test.go
+++ config/etcd_test.go
@@ -22,5 +22,5 @@ func TestGetEtcdMachines(t *testing.T) {
 	c := &Cluster{}
 	candy.Must(yaml.Unmarshal([]byte(ExampleYAML), c))
 	assert.Equal(t, c.GetEtcdMachines(),
-		"http://10.0.2.21:2379,http://10.0.2.22:2379")
+		"http://00-25-90-c0-f7-80:2379,http://00-25-90-c0-f6-ee:2379")
 }
