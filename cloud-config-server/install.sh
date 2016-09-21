94c741ec97e46c5199f0fa50690495d60b8fcf62
Qingsong Liu <liuqingsong@unisound.com>
2016-09-21 12:54:43 +0800

    Fix MAC address

--- cloud-config-server/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# FIXME: default to install coreos on /dev/sda
-default_iface=$(awk '$2 == 00000000 { print $1  }' /proc/net/route)
-mac_addr=`ip addr show dev "$default_iface" | awk '$1 ~ /^link\// { print $2 }'`
-wget --quiet -O ${mac_addr}.yml http://<HTTP_ADDR>/cloud-config/${mac_addr}
-sudo coreos-install -d /dev/sda -c ${mac_addr}.yml -b http://<HTTP_ADDR>/static -V current
-sudo reboot
