21fbe043a639ba909e9ac4199c5f990a57f6dd14
Yi Wang <yi.wang.2005@gmail.com>
2016-09-22 23:20:26 -0700

    Redefine the way to build bootstrapper Docker image

---
+++ docker/build.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if go install \
+      github.com/k8sp/sextant/cloud-config-server \
+      github.com/k8sp/sextant/addons; \
+then
+   cp $GOPATH/bin/{cloud-config-server,addons} .
+   docker build -t bootstrapper .
+fi
+       
+    
+    
+    
