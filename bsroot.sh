c29c864e55f26c0a3f7808ac824f92ae771e2c3d
yi.wu <yi.wu@baifendian.com>
2016-09-23 16:47:34 +0800

    mv install.sh zap lines to bsroot.sh

--- bsroot.sh
+++ bsroot.sh
@@ -193,6 +193,19 @@ prepare_cc_server_contents() {
     printf "Generating install.sh ... "
     cat > $BSROOT/html/static/cloud-config/install.sh <<EOF
 #!/bin/bash
+#Obtain devices
+devices=\$(lsblk -l |awk '\$6=="disk"{print \$1}')
+# Zap all devices
+# NOTICE: dd zero to device mbr will not affect parted printed table,
+#         so use parted to remove the part tables
+for d in \$devices
+do
+  for v_partition in \$(parted -s /dev/\${d} print|awk '/^ / {print \$1}')
+  do
+     parted -s /dev/\${d} rm \${v_partition}
+  done
+done
+
 # FIXME: default to install coreos on /dev/sda
 default_iface=\$(awk '\$2 == 00000000 { print \$1  }' /proc/net/route | uniq)
 
