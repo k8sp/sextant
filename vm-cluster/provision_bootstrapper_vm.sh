6970561b19a01419919efda300c87bbd807a80fe
zhengbo0 <bo.zheng@baifendian.com>
2016-09-26 22:18:06 +0800

    修改bug

--- vm-cluster/provision_bootstrapper_vm.sh
+++ vm-cluster/provision_bootstrapper_vm.sh
@@ -9,19 +9,7 @@ cat /root/.ssh/id_rsa.pub >> /root/.ssh/authorized_keys
 ssh_key=`cat /root/.ssh/authorized_keys` 
 sed -i -e 's#<SSH_KEY>#'"$ssh_key"'#' /bsroot/config/cluster-desc.yml
 
-#设置docker registry tls ca证书
-mkdir -p /etc/docker/certs.d/bootstrapper:5000
-cp  /bsroot/tls/ca.pem /etc/docker/certs.d/bootstrapper:5000/ca.crt
-systemctl daemon-reload
-systemctl restart docker
-
 #修复无法找到pxe　server的异常
 sed -i '/interface=eth0/,/bind-interfaces/d' /bsroot/config/dnsmasq.conf
-
-#启动dnsmasq,cloudconfig server,docker registry
-docker load < /bsroot/bootstrapper.tar
-docker run -d --net=host \
-  --privileged \
-  -v /var/run/docker.sock:/var/run/docker.sock \
-  -v /bsroot:/bsroot \
-  bootstrapper
+cd /bsroot
+./start_bootstrapper_container.sh /bsroot
