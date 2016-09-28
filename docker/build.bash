b1e5207e89ec4175451c0847acf653237b4c2711
Yi Wang <yi.wang.2005@gmail.com>
2016-09-27 23:41:15 -0700

    Edit build.bash to make sure it can run on Linux or non-Linux systems

--- docker/build.bash
+++ docker/build.bash
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-if go install \
+THIS_OS=$(go env | grep 'GOOS=' | cut -f 2 -d '=')
+
+if GOOS=linux GOARCH=amd64 go install \
       github.com/k8sp/sextant/cloud-config-server \
       github.com/k8sp/sextant/addons; \
 then
-   cp $GOPATH/bin/{cloud-config-server,addons} .
+   if [[ $THIS_OS != '"linux"' ]]; then
+       cp $GOPATH/bin/linux_amd64/{cloud-config-server,addons} .
+   else
+       cp $GOPATH/bin/{cloud-config-server,addons} .
+   fi
    docker build -t bootstrapper .
 fi
