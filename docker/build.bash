67bbbef7c405b18ee2e7168fe24db2e17ca52df6
Yi Wang <yi.wang.2005@gmail.com>
2016-09-27 23:46:36 -0700

    Also considers THIS_ARCH

--- docker/build.bash
+++ docker/build.bash
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 THIS_OS=$(go env | grep 'GOOS=' | cut -f 2 -d '=')
+THIS_ARCH=$(go env | grep 'GOARCH=' | cut -f 2 -d '=')
 
 if GOOS=linux GOARCH=amd64 go install \
       github.com/k8sp/sextant/cloud-config-server \
       github.com/k8sp/sextant/addons; \
 then
-   if [[ $THIS_OS != '"linux"' ]]; then
+   if [[ $THIS_OS != '"linux"' || $THIS_ARCH != '"amd64"' ]]; then
        cp $GOPATH/bin/linux_amd64/{cloud-config-server,addons} .
    else
        cp $GOPATH/bin/{cloud-config-server,addons} .
