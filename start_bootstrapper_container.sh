b9d42a0abe97837cfbd817941f24c6e6e21e3da6
zhengbo0 <bo.zheng@baifendian.com>
2016-09-28 17:01:38 +0800

    fix230

--- start_bootstrapper_container.sh
+++ start_bootstrapper_container.sh
@@ -1,32 +1,37 @@
-tart_bootstrapper_container.sh load docker image from bsroot and
-# then push them to registry
-if [ -z $1 ]; then
-  echo "Usage: start_bootstrapper_container.sh [/bsroot/complete/path]"
-  exit 1
+#!/bin/bash
+
+# start_bootstrapper_container.sh load docker image from bsroot and
+if [[ "$#" -gt 1 ]]; then
+    echo "Usage: start_bootstrapper_contaienr.sh [bsroot-path]"
+    exit 1
+elif [[ "$#" -ne 1 ]]; then
+    BSROOT=/bsroot
+else
+    BSROOT=$1
 fi
 
-if [[ $1 != /* ]]; then
-  echo "bsroot path not start with / !"
-  exit 1
+if [[ ! -d $BSROOT ]]; then
+    echo "$BSROOT is not a directory"
+    exit 2
 fi
 # push k8s images to registry from bsroot
-BOOTATRAPPER_DOMAIN=`grep "dockerdomain:" $1/config/cluster-desc.yml | awk '{print $2}' | sed 's/"//g' | sed 's/ //g'`
-HYPERKUBE_VERSION=`grep "hyperkube_version:" $1/config/cluster-desc.yml | awk '{print $2}' | sed 's/ //g' | sed -e 's/^"//' -e 's/"$//'`
-PAUSE_VERSION=`grep "pause_version:" $1/config/cluster-desc.yml | awk '{print $2}' | sed 's/ //g' | sed -e 's/^"//' -e 's/"$//'`
-FLANNEL_VERSION=`grep "flannel_version:" $1/config/cluster-desc.yml | awk '{print $2}' | sed 's/ //g' | sed -e 's/^"//' -e 's/"$//'`
+BOOTATRAPPER_DOMAIN=`grep "dockerdomain:" $BSROOT/config/cluster-desc.yml | awk '{print $2}' | sed 's/"//g' | sed 's/ //g'`
+HYPERKUBE_VERSION=`grep "hyperkube_version:" $BSROOT/config/cluster-desc.yml | awk '{print $2}' | sed 's/ //g' | sed -e 's/^"//' -e 's/"$//'`
+PAUSE_VERSION=`grep "pause_version:" $BSROOT/config/cluster-desc.yml | awk '{print $2}' | sed 's/ //g' | sed -e 's/^"//' -e 's/"$//'`
+FLANNEL_VERSION=`grep "flannel_version:" $BSROOT/config/cluster-desc.yml | awk '{print $2}' | sed 's/ //g' | sed -e 's/^"//' -e 's/"$//'`
 
 # Config Registry tls
 mkdir -p /etc/docker/certs.d/bootstrapper:5000
 rm -rf /etc/docker/certs.d/bootstrapper:5000/*
-cp $1/tls/ca.pem /etc/docker/certs.d/bootstrapper:5000/ca.crt
+cp $BSROOT/tls/ca.pem /etc/docker/certs.d/bootstrapper:5000/ca.crt
 
 echo "127.0.0.1 bootstrapper" >> /etc/hosts
 
-docker load < $1/bootstrapper.tar > /dev/null 2>&1 || { echo "Docker can not load bootstrapper.tar!"; exit 1; }
+docker load < $BSROOT/bootstrapper.tar > /dev/null 2>&1 || { echo "Docker can not load bootstrapper.tar!"; exit 1; }
 docker run -d --net=host \
   --privileged \
   -v /var/run/docker.sock:/var/run/docker.sock \
-  -v $1:/bsroot \
+  -v $BSROOT:/bsroot \
   bootstrapper
 
 # Sleep 3 seconds, waitting for registry started.
@@ -43,9 +48,7 @@ DOCKER_IMAGES=("typhoon1986/hyperkube-amd64:${HYPERKUBE_VERSION}" \
 len=${#DOCKER_IMAGES[@]}
 for ((i=0;i<len;i++)); do
   DOCKER_IMAGE=${DOCKER_IMAGES[i]}
-  #DOCKER_TAR_FILE=`echo $1/${DOCKER_IMAGE}.tar | sed "s/:/_/g" |awk -F'/' '{print $2}'`
-  DOCKER_TAR_FILE=`echo $1/${DOCKER_IMAGE}.tar | sed "s/:/_/g"`
-  echo $DOCKER_TAR_FILE
+  DOCKER_TAR_FILE=$BSROOT/$(echo ${DOCKER_IMAGE}.tar | sed "s/:/_/g" |awk -F'/' '{print $2}')
   DOCKER_TAG_NAME=`echo $BOOTATRAPPER_DOMAIN:5000/${DOCKER_IMAGE} | awk -F'/' '{print $1"/"$3}'`
   docker load < $DOCKER_TAR_FILE
   docker tag $DOCKER_IMAGE $DOCKER_TAG_NAME
