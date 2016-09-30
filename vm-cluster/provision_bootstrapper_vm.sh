bebede939106342b257778b138588dcddb4236c3
Yi Wang <yi.wang.2005@gmail.com>
2016-09-29 21:57:55 -0700

    Yi' proposal on the right order of file generations

--- vm-cluster/provision_bootstrapper_vm.sh
+++ vm-cluster/provision_bootstrapper_vm.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-#生成ssh key
-rm -rf /root/.ssh/
-/usr/bin/ssh-keygen -t rsa -f /root/.ssh/id_rsa -P ''
+# Copy VM keys out from /bsroot/vm-keys.
+mkdir -p /root/.ssh
+rm -rf /root/.ssh/*
+cp /bsroot/vm-keys/* /root/.ssh
 cat /root/.ssh/id_rsa.pub >> /root/.ssh/authorized_keys
-
-#设置bootstrapper免密码登陆其他虚拟机
-ssh_key=`cat /root/.ssh/authorized_keys` 
-sed -i -e 's#<SSH_KEY>#'"$ssh_key"'#' /bsroot/config/cluster-desc.yml
 
 #修复无法找到pxe　server的异常
 sed -i '/interface=eth0/,/bind-interfaces/d' /bsroot/config/dnsmasq.conf
+
 cd /bsroot
 ./start_bootstrapper_container.sh /bsroot
