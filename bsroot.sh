016a240e3cbf08b50b9562bc8584e0bb70cfb340
yi.wu <yi.wu@baifendian.com>
2016-10-21 19:56:24 +0800

    mid update

--- bsroot.sh
+++ bsroot.sh
@@ -246,14 +246,12 @@ EOF
 
 
 build_bootstrapper_image() {
+    local THIS_OS=$(go env | grep 'GOOS=' | cut -f 2 -d '=')
+    local THIS_ARCH=$(go env | grep 'GOARCH=' | cut -f 2 -d '=')
 
-    printf "Building sextant_build_image..."
-    docker build -t sextant_build_image $SEXTANT_DIR/build/build_image > /dev/null 2>&1 \
-      || { echo "build sextant_build_image failed"; exit 1;}
-    echo "Done"
-
+    # target binary arch is amd64, and build in docker image will always amd64
+    printf "Cross-compiling Sextant Go programs ... "
     docker rm sextant_build > /dev/null 2>&1
-
     docker run --name=sextant_build \
             --volume $SEXTANT_DIR:/go/src/github.com/k8sp/sextant \
             --volume $SEXTANT_DIR/docker:/go/bin \
@@ -264,7 +262,27 @@ build_bootstrapper_image() {
             go get github.com/k8sp/sextant/cloud-config-server github.com/k8sp/sextant/addons \
             || { echo "Build sextant failed..."; exit 1; }
     docker rm sextant_build > /dev/null 2>&1
+    echo "Done"
 
+    # FIXME: build addon for this arch, only support MacOS and linux
+    printf "Compiling addons for local machine ... "
+    if [[ $THIS_OS != '"linux"' || $THIS_ARCH != '"amd64"' ]]; then
+      ADDONS=$SEXTANT_DIR/docker/addons
+    else
+      docker run --name=sextant_build \
+              --volume $SEXTANT_DIR:/go/src/github.com/k8sp/sextant \
+              --volume $SEXTANT_DIR/docker:/go/bin \
+              -e CGO_ENABLED=0 \
+              -e GOOS=$THIS_OS \
+              -e GOARCH=$THIS_ARCH \
+              golang:wheezy \
+              go get github.com/k8sp/sextant/addons \
+              || { echo "Build addon for local arch failed..."; exit 1; }
+      ADDONS=$SEXTANT_DIR/docker/$THIS_OS_$THIS_ARCH/addons
+    fi
+    echo "Done"
+
+    printf "Cross-compiling Docker registry ... "
     docker rm registry_build > /dev/null 2>&1
     docker run --name=registry_build \
             --volume $SEXTANT_DIR/docker:/go/bin \
@@ -273,10 +291,9 @@ build_bootstrapper_image() {
             -e GOARCH=amd64 \
             golang:wheezy \
             sh -c "go get -u -d github.com/docker/distribution/cmd/registry && cd /go/src/github.com/docker/distribution && make PREFIX=/go clean /go/bin/registry >/dev/null" \
-            || { echo "go get registry failed..."; exit 1; }
-
-    docker stop registry_build
+            || { echo "Complie Docker registry failed..."; exit 1; }
     docker rm registry_build > /dev/null 2>&1
+    echo "Done"
 
     printf "Building bootstrapper image ... "
     cd $SEXTANT_DIR/docker
@@ -341,27 +358,23 @@ prepare_setup_kubectl() {
 }
 
 generate_addons_config() {
-    printf "Building addons..."
-    rm $GOPATH/bin/addons >/dev/null 2>&1
-    go install github.com/k8sp/sextant/addons || { echo "Failed"; exit 1; }
-    echo "Done"
     printf "Generating configuration files ..."
-    $GOPATH/bin/addons -cluster-desc-file $CLUSTER_DESC \
+    $ADDONS -cluster-desc-file $CLUSTER_DESC \
         -template-file $SEXTANT_DIR/addons/template/ingress.template \
         -config-file $BSROOT/html/static/ingress.yaml || \
         { echo 'Failed to generate ingress.yaml !' ; exit 1; }
 
-    $GOPATH/bin/addons -cluster-desc-file $CLUSTER_DESC \
+    $ADDONS -cluster-desc-file $CLUSTER_DESC \
         -template-file $SEXTANT_DIR/addons/template/skydns.template \
         -config-file $BSROOT/html/static/skydns.yaml || \
         { echo 'Failed to generate skydns.yaml !' ; exit 1; }
 
-    $GOPATH/bin/addons -cluster-desc-file $CLUSTER_DESC \
+    $ADDONS -cluster-desc-file $CLUSTER_DESC \
         -template-file $SEXTANT_DIR/addons/template/skydns-service.template \
         -config-file $BSROOT/html/static/skydns-service.yaml || \
         { echo 'Failed to generate skydns-service.yaml !' ; exit 1; }
 
-    $GOPATH/bin/addons -cluster-desc-file $CLUSTER_DESC \
+    $ADDONS -cluster-desc-file $CLUSTER_DESC \
         -template-file $SEXTANT_DIR/addons/template/dnsmasq.conf.template \
         -config-file $BSROOT/config/dnsmasq.conf || \
         { echo 'Failed to generate dnsmasq.conf !' ; exit 1; }
