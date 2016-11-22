35ae730c12d5510b75e2bbcaf1597fce5ab3a171
liangjiameng <liangjiameng@unisound.com>
2016-11-22 14:35:59 +0800

    fix yaml parser

--- scripts/bsroot_lib.bash
+++ scripts/bsroot_lib.bash
@@ -43,6 +43,30 @@ function parse_yaml() {
     }' | sed 's/_=/+=/g'
 }
 
+## derived from https://gist.github.com/epiloque/8cf512c6d64641bde388
+## works for arrays of hashes, as long as the hashes do not have arrays
+parse_yaml2() {
+    local prefix=$2
+    local s
+    local w
+    local fs
+    s='[[:space:]]*'
+    w='[a-zA-Z0-9_]*'
+    fs="$(echo @|tr @ '\034')"
+    sed -ne "s|^\($s\)\($w\)$s:$s\"\(.*\)\"$s\$|\1$fs\2$fs\3|p" \
+        -e "s|^\($s\)\($w\)$s[:-]$s\(.*\)$s\$|\1$fs\2$fs\3|p" "$1" |
+    awk -F"$fs" '{
+      indent = length($1)/2;
+      if (length($2) == 0) { conj[indent]="+";} else {conj[indent]="";}
+      vname[indent] = $2;
+      for (i in vname) {if (i > indent) {delete vname[i]}}
+      if (length($3) > 0) {
+              vn=""; for (i=0; i<indent; i++) {vn=(vn)(vname[i])("_")}
+              printf("%s%s%s%s=(\"%s\")\n", "'"$prefix"'",vn, $2, conj[indent-1],$3);
+      }
+    }' | sed 's/_=/+=/g'
+}
+
 # load_yaml calls parse_yaml to convert a .yaml file into a temporary
 # .bash file, run the temporary .bash file, and delete it.  So we will
 # have bash variables defined and whose values are values in the .yaml
@@ -55,7 +79,7 @@ function load_yaml() {
     local yaml=$1
     local prefix=$2
     local parsedYaml=$(mktemp /tmp/bsroot-parse-yaml.XXXXX)
-    parse_yaml $yaml $prefix > $parsedYaml
+    parse_yaml2 $yaml $prefix > $parsedYaml
     source $parsedYaml
     rm $parsedYaml
 }
