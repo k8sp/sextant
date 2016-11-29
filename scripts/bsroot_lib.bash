197fcb78d9dfc60b5f1ddcae5ee1820f68c9abf5
Liang Jiameng <liangjiameng@unisound.com>
2016-11-29 17:09:34 +0800

    rename function parse_yaml2 to parse_yaml

--- scripts/bsroot_lib.bash
+++ scripts/bsroot_lib.bash
@@ -24,28 +24,10 @@ function check_prerequisites() {
 # 
 #    parse_yaml example.yaml "cluster_desc_" > parseResult.bash
 # 
-function parse_yaml() {
-    local yaml=$1
-    local prefix=$2
-    local s='[[:space:]]*'
-    local w='[a-zA-Z0-9_]*'
-    local fs="$(echo @|tr @ '\034')"
-    sed -ne "s|^\($s\)\($w\)$s:$s\"\(.*\)\"$s\$|\1$fs\2$fs\3|p" \
-        -e "s|^\($s\)\($w\)$s[:-]$s\(.*\)$s\$|\1$fs\2$fs\3|p" $yaml |
-    awk -F"$fs" '{
-    indent = length($1)/2;
-    vname[indent] = $2;
-    for (i in vname) {if (i > indent) {delete vname[i]}}
-        if (length($3) > 0) {
-            vn=""; for (i=0; i<indent; i++) {vn=(vn)(vname[i])("_")}
-            printf("%s%s%s=(\"%s\")\n", "'"$prefix"'",vn, $2, $3);
-        }
-    }' | sed 's/_=/+=/g'
-}
 
 ## derived from https://gist.github.com/epiloque/8cf512c6d64641bde388
 ## works for arrays of hashes, as long as the hashes do not have arrays
-parse_yaml2() {
+parse_yaml() {
     local prefix=$2
     local s
     local w
@@ -79,7 +61,7 @@ function load_yaml() {
     local yaml=$1
     local prefix=$2
     local parsedYaml=$(mktemp /tmp/bsroot-parse-yaml.XXXXX)
-    parse_yaml2 $yaml $prefix > $parsedYaml
+    parse_yaml $yaml $prefix > $parsedYaml
     source $parsedYaml
     rm $parsedYaml
 }
