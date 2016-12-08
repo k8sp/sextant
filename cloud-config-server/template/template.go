b3681e36632e0f613651293016ca6bb12190288f
yi.wu <yi.wu@baifendian.com>
2016-12-08 10:32:09 +0800

    make cc template from multiple files

--- cloud-config-server/template/template.go
+++ cloud-config-server/template/template.go
@@ -7,8 +7,9 @@ import (
 	"text/template"
 
 	"github.com/k8sp/sextant/cloud-config-server/certgen"
-	tpcfg "github.com/k8sp/sextant/clusterdesc"
+	"github.com/k8sp/sextant/cloud-config-server/clusterdesc"
 	"github.com/topicai/candy"
+	"gopkg.in/yaml.v2"
 )
 
 // ExecutionConfig struct config a Coreos's cloud config file which use for installing Coreos in k8s cluster.
@@ -43,10 +44,27 @@ type ExecutionConfig struct {
 	OSName                   string
 }
 
-// Execute returns the executed cloud-config template for a node with
-// given MAC address.
-func Execute(tmpl *template.Template, config *tpcfg.Cluster, mac, caKey, caCrt string, w io.Writer) error {
-	node := getNodeByMAC(config, mac)
+// Execute load template files from "ccTemplateDir", parse clusterDescFile to
+// "clusterdesc.Cluster" struct and then run the templateName
+func Execute(w io.Writer, mac, templateName, ccTemplateDir, clusterDescFile, caKey, caCrt string) error {
+	// Load data from file every time, no need to read from remote url
+	t, parseErr := template.ParseGlob(ccTemplateDir + "/*")
+	if parseErr != nil {
+		return parseErr
+	}
+	clusterDescBuff, readErr := ioutil.ReadFile(clusterDescFile)
+	if readErr != nil {
+		return readErr
+	}
+	c := &clusterdesc.Cluster{}
+	candy.Must(yaml.Unmarshal(clusterDescBuff, c))
+	confData := GetConfigDataByMac(mac, c, caKey, caCrt)
+	return t.ExecuteTemplate(w, "cc-template", *confData)
+}
+
+// GetConfigDataByMac returns data struct for cloud-config template to execute
+func GetConfigDataByMac(mac string, clusterdesc *clusterdesc.Cluster, caKey, caCrt string) *ExecutionConfig {
+	node := getNodeByMAC(clusterdesc, mac)
 	ca, e := ioutil.ReadFile(caCrt)
 	candy.Must(e)
 
@@ -55,45 +73,44 @@ func Execute(tmpl *template.Template, config *tpcfg.Cluster, mac, caKey, caCrt s
 		k, c = certgen.Gen(true, node.Hostname(), caKey, caCrt)
 	}
 
-	ec := ExecutionConfig{
+	return &ExecutionConfig{
 		Hostname:                 node.Hostname(),
 		CephMonitor:              node.CephMonitor,
 		KubeMaster:               node.KubeMaster,
 		EtcdMember:               node.EtcdMember,
 		IngressLabel:             node.IngressLabel,
 		FlannelIface:             node.FlannelIface,
-		InitialCluster:           config.InitialEtcdCluster(),
-		SSHAuthorizedKeys:        config.SSHAuthorizedKeys,
-		MasterHostname:           config.GetMasterHostname(),
-		EtcdEndpoints:            config.GetEtcdEndpoints(),
-		BootstrapperIP:           config.Bootstrapper,
-		Dockerdomain:             config.Dockerdomain,
-		K8sClusterDNS:            config.K8sClusterDNS,
-		K8sServiceClusterIPRange: config.K8sServiceClusterIPRange,
-		ZapAndStartOSD:           config.Ceph.ZapAndStartOSD,
-		Images:                   config.Images,
+		InitialCluster:           clusterdesc.InitialEtcdCluster(),
+		SSHAuthorizedKeys:        clusterdesc.SSHAuthorizedKeys,
+		MasterHostname:           clusterdesc.GetMasterHostname(),
+		EtcdEndpoints:            clusterdesc.GetEtcdEndpoints(),
+		BootstrapperIP:           clusterdesc.Bootstrapper,
+		Dockerdomain:             clusterdesc.Dockerdomain,
+		K8sClusterDNS:            clusterdesc.K8sClusterDNS,
+		K8sServiceClusterIPRange: clusterdesc.K8sServiceClusterIPRange,
+		ZapAndStartOSD:           clusterdesc.Ceph.ZapAndStartOSD,
+		Images:                   clusterdesc.Images,
 		// Mulit-line context in yaml should keep the indent,
 		// there is no good idea for templaet package to auto keep the indent so far,
 		// so insert 6*whitespace at the begging of every line
 		CaCrt:             strings.Join(strings.Split(string(ca), "\n"), "\n      "),
 		Crt:               strings.Join(strings.Split(string(c), "\n"), "\n      "),
 		Key:               strings.Join(strings.Split(string(k), "\n"), "\n      "),
-		FlannelBackend:    config.FlannelBackend,
-		RebootStrategy:    config.CoreOS.RebootStrategy,
-		StartTime:         config.CoreOS.StartTime,
-		TimeLength:        config.CoreOS.TimeLength,
-		CoreOSVersion:     config.CoreOSVersion,
-		GPUDriversVersion: config.GPUDriversVersion,
-		OSName:            config.OSName,
+		FlannelBackend:    clusterdesc.FlannelBackend,
+		RebootStrategy:    clusterdesc.CoreOS.RebootStrategy,
+		StartTime:         clusterdesc.CoreOS.StartTime,
+		TimeLength:        clusterdesc.CoreOS.TimeLength,
+		CoreOSVersion:     clusterdesc.CoreOSVersion,
+		GPUDriversVersion: clusterdesc.GPUDriversVersion,
+		OSName:            clusterdesc.OSName,
 	}
-	return tmpl.Execute(w, ec)
 }
 
-func getNodeByMAC(c *tpcfg.Cluster, mac string) tpcfg.Node {
+func getNodeByMAC(c *clusterdesc.Cluster, mac string) clusterdesc.Node {
 	for _, n := range c.Nodes {
 		if n.MAC == mac {
 			return n
 		}
 	}
-	return tpcfg.Node{MAC: mac, CephMonitor: false, KubeMaster: false, EtcdMember: false}
+	return clusterdesc.Node{MAC: mac, CephMonitor: false, KubeMaster: false, EtcdMember: false}
 }
--- clusterdesc/README.md
+++
@@ -1,5 +0,0 @@
-# `clusterdesc`
-
-The Go package `clusterdesc` defines the schema of
-`cluster-desc.yaml`, the file describes the Kubernetes cluster and is
-used as the input to `bsroot.sh`, `cloud-config-server` and `addons`.
