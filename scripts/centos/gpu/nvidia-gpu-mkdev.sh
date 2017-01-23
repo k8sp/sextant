310f852847b8c0d8e4b8b903a8626c713e0584f5
Qingsong Liu <liuqingsong@unisound.com>
2017-01-23 15:43:14 +0800

    Refactor the code of centos gpu

---
+++ scripts/centos/gpu/nvidia-gpu-mkdev.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+/sbin/modprobe nvidia
+# Count the number of NVIDIA controllers found.
+NVDEVS=$(lspci | grep -i NVIDIA)
+N3D=`echo "$NVDEVS" | grep "3D controller" | wc -l`
+NVGA=`echo "$NVDEVS" | grep "VGA compatible controller" | wc -l`
+N=`expr $N3D + $NVGA - 1`
+for i in `seq 0 $N`; do
+    mknod -m 666 /dev/nvidia$i c 195 $i
+done
+mknod -m 666 /dev/nvidiactl c 195 255
+/sbin/modprobe nvidia-uvm
+# Find out the major device number used by the nvidia-uvm driver
+D=$(grep nvidia-uvm /proc/devices | awk '{print \$1}');
+mknod -m 666 /dev/nvidia-uvm c \$D 0
