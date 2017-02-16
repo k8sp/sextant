bda6e4389c25ca35ea3b86294fc660ee8ea9ff79
Qingsong Liu <liuqingsong@unisound.com>
2017-02-16 18:27:34 +0800

    Refactor Nvidia GPU code

--- scripts/centos/gpu/build_centos_gpu_drivers.sh
+++ scripts/centos/gpu/build_centos_gpu_drivers.sh
@@ -7,66 +7,53 @@ set -xe
 
 DRIVER_VERSION=$1
 HTTP_GPU_DIR=$2
-CENTOS_VERSION=$3
-DRIVER_ARCHIVE=NVIDIA-Linux-x86_64-${DRIVER_VERSION}
-DRIVER_ARCHIVE_PATH=${PWD}/nvidia_installers/${DRIVER_ARCHIVE}.run
-WORK_DIR=${PWD}/run_files/${CENTOS_VERSION}
-ARTIFACT_DIR=${WORK_DIR}/${DRIVER_ARCHIVE}
+NVIDIA_RUN_NAME=NVIDIA-Linux-x86_64-${DRIVER_VERSION}
+NVIDIA_RUN_FILE=${NVIDIA_RUN_NAME}.run
 NVIDIA_DIR=/usr/local/nvidia
 NVIDIA_BIN_DIR=/usr/local/nvidia/bin
 NVIDIA_LIB_DIR=/usr/local/nvidia/lib64
+LIBS_FILES=libraries-${DRIVER_VERSION}.tar.bz2
+TOOLS_FILES=tools-${DRIVER_VERSION}.tar.bz2
 TOOLS="nvidia-debugdump nvidia-cuda-mps-control nvidia-xconfig nvidia-modprobe nvidia-smi nvidia-cuda-mps-server nvidia-persistenced nvidia-settings"
 
-download_nvidia_gpu_drivers(){
-  if [ ! -f ${DRIVER_ARCHIVE_PATH} ]
+build_lib_and_ko() {
+  if [ ! -f ${NVIDIA_RUN_FILE} ]
   then
     echo Downloading NVIDIA Linux drivers version ${DRIVER_VERSION}
-    mkdir -p nvidia_installers
-    wget --quiet -c -N -P nvidia_installers ${HTTP_GPU_DIR}/${DRIVER_ARCHIVE}.run || { echo "Failed"; exit 1; }
+    wget --quiet -c -N -O ${NVIDIA_RUN_FILE} ${HTTP_GPU_DIR}/${NVIDIA_RUN_FILE}
   fi
-}
 
-build_lib_and_ko() {
-  mkdir -p ${WORK_DIR}
-  cp -ul ${DRIVER_ARCHIVE_PATH} ${WORK_DIR}
-  pushd ${WORK_DIR}
-  chmod +x ${DRIVER_ARCHIVE}.run
-  rm -Rf ./${DRIVER_ARCHIVE}
-  ./${DRIVER_ARCHIVE}.run -x
-  cd ${DRIVER_ARCHIVE}
-  ./nvidia-installer -s
-  popd
+  chmod +x ./${NVIDIA_RUN_FILE}
+  rm -rf ${NVIDIA_RUN_NAME}
+  ./${NVIDIA_RUN_FILE} -x
+  ${NVIDIA_RUN_NAME}/nvidia-installer -s
   # Create archives with no paths
-  tar -C ${ARTIFACT_DIR} -cvj $(basename -a ${ARTIFACT_DIR}/*.so.*) > libraries-${DRIVER_VERSION}.tar.bz2
-  tar -C ${ARTIFACT_DIR} -cvj ${TOOLS} > tools-${DRIVER_VERSION}.tar.bz2
+  tar -C ${NVIDIA_RUN_NAME} -cvj $(basename -a ${NVIDIA_RUN_NAME}/*.so.*) > $LIBS_FILES
+  tar -C ${NVIDIA_RUN_NAME} -cvj ${TOOLS} > $TOOLS_FILES
 }
 
 install_lib_and_ko() {
-  mkdir -p ${NVIDIA_DIR}
     mkdir -p ${NVIDIA_BIN_DIR}
-    cp ./tools-${DRIVER_VERSION}.tar.bz2 ${NVIDIA_BIN_DIR}
-    pushd ${NVIDIA_BIN_DIR}
-    tar -xjf ./tools-${DRIVER_VERSION}.tar.bz2
-    popd
+    tar -xjf $TOOLS_FILES -C ${NVIDIA_BIN_DIR}
     mkdir -p ${NVIDIA_LIB_DIR}
-    cp ./libraries-${DRIVER_VERSION}.tar.bz2 ${NVIDIA_LIB_DIR}
-    pushd ${NVIDIA_LIB_DIR}
+    tar -xjf $LIBS_FILES -C ${NVIDIA_LIB_DIR}
+
     for LIBRARY_NAME in libcuda libGLESv1_CM \
       libGL libEGL \
       libnvidia-cfg libnvidia-encode libnvidia-fbc \
       libnvidia-ifr libnvidia-ml libnvidia-opencl \
       libnvcuvid libvdpau
     do
-      ln -sf ${LIBRARY_NAME}.so.${DRIVER_VERSION} ${LIBRARY_NAME}.so.1
-      ln -sf ${LIBRARY_NAME}.so.1 ${LIBRARY_NAME}.so
+      ln -sf $NVIDIA_LIB_DIR/${LIBRARY_NAME}.so.${DRIVER_VERSION} $NVIDIA_LIB_DIR/${LIBRARY_NAME}.so.1
+      ln -sf $NVIDIA_LIB_DIR/${LIBRARY_NAME}.so.1 $NVIDIA_LIB_DIR/${LIBRARY_NAME}.so
     done
-    ln -sf libOpenCL.so.1.0.0 libOpenCL.so.1
-    ln -sf libOpenCL.so.1 libOpenCL.so
-    ln -sf libGLESv2.so.${DRIVER_VERSION} libGLESv2.so.2
-    ln -sf libGLESv2.so.2 libGLESv2.so
-    ln -sf libvdpau_nvidia.so.${DRIVER_VERSION} libvdpau_nvidia.so
-    ln -sf libvdpau_trace.so.${DRIVER_VERSION} libvdpau_trace.so
-    tar -xjf ./libraries-${DRIVER_VERSION}.tar.bz2
+
+    ln -sf $NVIDIA_LIB_DIR/libOpenCL.so.1.0.0 $NVIDIA_LIB_DIR/libOpenCL.so.1
+    ln -sf $NVIDIA_LIB_DIR/libOpenCL.so.1 $NVIDIA_LIB_DIR/libOpenCL.so
+    ln -sf $NVIDIA_LIB_DIR/libGLESv2.so.${DRIVER_VERSION} $NVIDIA_LIB_DIR/libGLESv2.so.2
+    ln -sf $NVIDIA_LIB_DIR/libGLESv2.so.2 $NVIDIA_LIB_DIR/libGLESv2.so
+    ln -sf $NVIDIA_LIB_DIR/libvdpau_nvidia.so.${DRIVER_VERSION} $NVIDIA_LIB_DIR/libvdpau_nvidia.so
+    ln -sf $NVIDIA_LIB_DIR/libvdpau_trace.so.${DRIVER_VERSION} $NVIDIA_LIB_DIR/libvdpau_trace.so
 }
 
 disable_nouveau() {
@@ -79,8 +66,7 @@ EOF
 }
 
 
-disable_nouveau
-download_nvidia_gpu_drivers
+#disable_nouveau
 build_lib_and_ko
 install_lib_and_ko
 
