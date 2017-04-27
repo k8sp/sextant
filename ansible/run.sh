7b96a71d5871201f6ce55848c863874aed4b6338
Hui Zhang <zhanghui@unisound.com>
2017-04-27 15:01:57 +0800

    add centos Role, finished cloud-init replace.

--- ansible/run.sh
+++ ansible/run.sh
@@ -1,7 +1,26 @@
 #!/usr/bin/env bash
 
 set -x
+set -e
 set -o nounset
 
-ansible-playbook site.yaml -f 5 -i stage #--list-hosts
+if [[ $# == 0 ]]; then
+    echo "usage: $0 [run|check]"
+    exit 1
+fi
+
+case $1 in
+    check)
+        ansible-playbook site.yml -f 5 -i staging/hosts --syntax-check 
+        ansible-playbook site.yml -f 5 -i staging/hosts --list-hosts 
+        ansible-playbook site.yml -f 5 -i staging/hosts --list-tasks
+        ;;
+    run)
+        ansible-playbook site.yml -f 5 -i staging/hosts 
+        ;;
+    *)
+        echo "usage: $0 [run|check]"
+        exit 1
+        ;;
+esac
 
