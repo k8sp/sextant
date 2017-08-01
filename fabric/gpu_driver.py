4675261626741cb4fd57c7bd553bd8b2134bc4be
gongweibao <weibao.gong@gmail.com>
2017-08-01 09:35:59 +0000

    add gpu

---
+++ fabric/gpu_driver.py
@@ -0,0 +1,43 @@
+from __future__ import with_statement
+from fabric.api import *
+from fabric.contrib.console import confirm
+import fabric.operations as op
+import yaml
+import sys
+
+driver_version=""
+http_gpu_dir=""
+boot_strapper=""
+
+def prepare():
+    cmd = """sed -i 's/^SELINUX=.*/SELINUX=disabled/g' /etc/selinux/config && cat /etc/selinux/config
+        && reboot"""
+    run(cmd)
+
+def install():
+    # Imporant: gpu must be installed after the kernel has been installed
+    run("wget -P /root %s/build_centos_gpu_drivers.sh" % http_gpu_dir)
+    cmd = "bash -x /root/build_centos_gpu_drivers.sh %s %s" % (driver_version, http_gpu_dir)
+    run(cmd)
+
+def check():
+    cmd="if [[ ! nvidia-smi |grep \"Driver Version\" | grep \"%s\" ]]; then exit 1; fi " % driver_version
+    result = run(cmd)
+    if result.failed:
+        abort("check failed")
+
+with open("hosts.yaml", 'r') as stream:
+    try:
+        y = yaml.load(stream)
+        env.hosts = y["hosts"]
+        env.user = y["user"]
+        env.password = y["password"]
+
+        boot_strapper = y["boot_strapper"]
+        driver_version = y["gpu"]["driver_version"]
+        http_gpu_dir="http://%s/static/CentOS7/gpu_drivers" % boot_strapper
+    except yaml.YAMLError as exc:
+        print(exc)
+        abort("load yaml error")
+
+
