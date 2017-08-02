b0830b44fdf81971cfe209d97f788d68e01bd9f2
gongweibao <weibao.gong@gmail.com>
2017-08-02 07:55:57 +0000

    add log.sh

---
+++ scripts/log.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+loglevel=0 #debug:0; info:1; warn:2; error:3; fatal:4
+function log {
+        local msg;local logtype
+        logtype=$1
+        msg=$2
+        datetime=`date +'%F %H:%M:%S'`
+        logformat="[${logtype}]${datetime} funcname: ${FUNCNAME[@]/log/}  [line:`caller 0 | awk '{print$1}'`]\t${msg}"
+        {
+        case $logtype in
+                debug)
+                        [[ $loglevel -le 0 ]] && echo -e "\033[30m${logformat}\033[0m" ;;
+                info)
+                        [[ $loglevel -le 1 ]] && echo -e "\033[32m${logformat}\033[0m" ;;
+                warn)
+                        [[ $loglevel -le 2 ]] && echo -e "\033[33m${logformat}\033[0m" ;;
+                error)
+                        [[ $loglevel -le 3 ]] && echo -e "\033[31m${logformat}\033[0m" ;;
+                fatal)
+                        [[ $loglevel -le 4 ]] && echo -e "\033[31m${logformat}\033[0m" ; exit 1; ;;
+
+        esac
+        } 
+}
+
+#
+#Wlog(){
+#    log warn "$@"
+#}
+#
+#Ilog(){
+#    log info "$@"
+#}
+#
+#Elog(){
+#    log error "$@"
+#}
+
+log info "this's info log"
+log error "this's error log"
+log warn "this's warn log"
+
+Wlog "this's warn log"
+Ilog "this's info log"
+Elog "this's warn log"
